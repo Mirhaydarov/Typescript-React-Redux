440e8dd86240638b26601aa413c7729f167a61c6
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-10-28 11:42:15 +0500

    feat: add route Nav, UserList, UserProfile, in routes

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -5,13 +5,25 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 // Route path
 import { book } from './book';
 
+// Components
+import { Nav } from '../components/Nav';
+import { UserList } from '../bus/UserList';
+import { UserProfile } from '../components/UserProfile';
+
 export const pageNotFound = (): ReactElement => (
   <h2>404: Not Found</h2>
 );
 
 export const Routes: FC = (): ReactElement => (
   <>
+    <Route component={Nav} path={book.root} />
     <Switch>
+      <Route exact component={UserList} path={book.root} />
+      <Route
+        exact
+        component={UserProfile}
+        path={`${book.profile}/:userName`}
+      />
       <Route component={pageNotFound} />
       <Redirect to={book.root} />
     </Switch>
